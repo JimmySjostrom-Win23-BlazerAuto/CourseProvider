9d586d8a1f59248ec01ac489889cdb5e049c650c
sniffmcmuffin <sjostrom.jimmy@gmail.com>
2024-05-25 10:27:56 +0200

    Add project files.

---
+++ Infrastructure/GraphQL/Queries/CourseQuery.cs
@@ -0,0 +1,21 @@
+﻿using Infrastructure.Interfaces;
+using Infrastructure.Models;
+
+namespace Infrastructure.GraphQL.Queries;
+
+public class CourseQuery(ICourseService courseService)
+{
+    public readonly ICourseService _courseService = courseService;
+
+    [GraphQLName("getCourses")]
+    public async Task<IEnumerable<Course>> GetCourses()
+    {
+        return await _courseService.GetCoursesAsync();
+    }
+
+    [GraphQLName("getCourseById")]
+    public async Task<Course> GetCourseByIdAsync(string id)
+    {
+        return await _courseService.GetCourseByIdAsync(id);
+    }
+}
---
+++ Infrastructure/Interfaces/ICourseService.cs
@@ -0,0 +1,13 @@
+﻿using Infrastructure.Models;
+
+namespace Infrastructure.Interfaces;
+
+public interface ICourseService
+{
+    Task<Course> CreateCourseAsync(CourseCreateRequest request);
+    Task<Course> GetCourseByIdAsync(string id);
+    Task<IEnumerable<Course>> GetCoursesAsync();
+    Task<Course> UpdateCourseAsync(CourseUpdateRequest request);
+    Task<bool> DeleteCourseAysnc(string id);
+
+}
